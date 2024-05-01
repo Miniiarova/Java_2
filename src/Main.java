e9c1497675501a5179f1024bff70e72271a7c1c4
MarinaM <marina.miniiarova@gmail.com>
2024-05-01 20:18:47 +0700

    добавлен расчет бонусов

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+       int price = 100;
+        int rubles_per_mile = 20;
+        int bonus = price/rubles_per_mile;
+        System.out.println(bonus);
+    }
+}
